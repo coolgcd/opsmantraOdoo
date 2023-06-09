67759c517673d9f52eb6de0fc15d394f44e4e7f5
sanjeev1221 <sanjeev.planetodoo@gmail.com>
2023-06-09 22:40:20 +0530

    apps menu hide added title module

---
+++ mantra/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'AMJAS',
+    'version': '1.1',
+    'summary': 'Mantra Project',
+    'sequence': 10,
+    'description': """ """,
+    'category': '',
+    'website': 'https://www.odoo.com/app/',
+    'depends': ["base"],
+    'data': [
+        'views/action_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+}
